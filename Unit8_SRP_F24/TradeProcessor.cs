1c426ccacd7d941aa5327fcecb1bb35ae85c332e
Lonnie Golen <lgolen@css.edu>
2024-10-27 15:39:56 -0500

    Updates for unit 8 learning activity

--- Unit8_SRP_F24/TradeProcessor.cs
+++ Unit8_SRP_F24/TradeProcessor.cs
@@ -121,16 +121,17 @@ namespace SingleResponsibilityPrinciple
             //    The @ sign allows for back slashes
             //    Watch for double quotes which must be escaped using "" 
             //    Watch for extra spaces after C: and avoid paths with - hyphens -
-            string genericConnectString = @"Data Source=(local);Initial Catalog=TradeDatabase;Integrated Security=True;";
+            //string genericConnectString = @"Data Source=(local);Initial Catalog=TradeDatabase;Integrated Security=True;";
             // The datadirConnectString connection string uses |DataDirectory| 
             //    and assumes the tradedatabase.mdf file is stored in 
             //    SingleResponsibilityPrinciple\bin\Debug 
-            string datadirConnectString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\tradedatabase.mdf;Integrated Security=True;Connect Timeout=30;";
+
+            string datadirConnectString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\golen\OneDrive\Documents\tradedatabase.mdf;Integrated Security=True;Connect Timeout=30;";
             // This users the Azure connection string
-            string azureConnectString = @"Data Source=cis3115-server.database.windows.net;Initial Catalog=CIS3115;User ID=cis3115;Password=Saints4SQL;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //string azureConnectString = @"Data Source=cis3115-server.database.windows.net;Initial Catalog=CIS3115;User ID=cis3115;Password=Saints4SQL;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
            
             // Change the connection string used to match the one you want
-            using (var connection = new SqlConnection(genericConnectString))
+            using (var connection = new SqlConnection(datadirConnectString))
             {
                 LogMessage("INFO:Going to open database connection");
                 connection.Open();
---
+++ Unit8_SRP_F24Tests/TradeProcessorTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SingleResponsibilityPrinciple;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple.Tests
+{
+    [TestClass()]
+    public class TradeProcessorTests
+    {
+        private int CountDbRecords()
+        {
+            string datadirConnectString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\golen\OneDrive\Documents\tradedatabase.mdf;Integrated Security=True;Connect Timeout=30;";
+            // Change the connection string used to match the one you want
+            using (var connection = new SqlConnection(datadirConnectString))
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                string myScalarQuery = "SELECT COUNT(*) FROM trade";
+                SqlCommand myCommand = new SqlCommand(myScalarQuery, connection);
+                //myCommand.Connection.Open();
+                int count = (int)myCommand.ExecuteScalar();
+                connection.Close();
+                return count;
+            }
+        }
+
+        [TestMethod()]
+        public void TestNormalFile()
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Unit8_SRP_F24Tests.goodtrades.txt");
+            var tradeProcessor = new TradeProcessor();
+
+            //Act
+            int countBefore = CountDbRecords();
+            tradeProcessor.ProcessTrades(tradeStream);
+            //Assert
+            int countAfter = CountDbRecords();
+            Assert.AreEqual(countBefore + 1, countAfter);
+        }
+    }
+}
